dbf1221e5d55a797e91e033e9a393d1337984c62
paul.v.estipona.ctr <paul_estipona@yahoo.com>
2023-05-31 20:18:47 -0400

    Edited README.md to Project in GitHub

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # GitHub Portfolio Project - Front End Banking Application:
 
+## Resources:
+
+### Bootstrap Resource pages:
+* [Introduction to Bootstrap](https://getbootstrap.com/docs/4.3/getting-started/introduction/)
+* [Bootstrap NavBar](https://getbootstrap.com/docs/5.3/components/navbar/)
+* [Bootstrap Cards](https://getbootstrap.com/docs/5.3/components/card/#about)
+* [Bootstrap Tooltips](https://getbootstrap.com/docs/4.0/components/tooltips/)
+* [Bootstrap Tables](https://getbootstrap.com/docs/4.0/content/tables/)
+* [Background color](https://getbootstrap.com/docs/5.0/utilities/background/)
+
+### React Resource Pages:
+* [React \<input> Component](https://react.dev/reference/react-dom/components/input#controlling-an-input-with-a-state-variable)
+* [Conditional Rendering](https://legacy.reactjs.org/docs/conditional-rendering.html)
+
+### MDN Web Docs:
+* [HTML elements reference](https://developer.mozilla.org/en-US/docs/Web/HTML/Element)
+  * [\<p>: The Paragraph element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/p)
+* [Event](https://developer.mozilla.org/en-US/docs/Web/API/Event)
+* [HashChangeEvent](https://developer.mozilla.org/en-US/docs/Web/API/HashChangeEvent)
+* [isNaN()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN)
--- alldata.js
+++ alldata.js
@@ -3,6 +3,41 @@ function AllData(){
     return (
         <h1>All Data:<br/>
             {JSON.stringify(ctx)}<br/>
+            <table className="table table-striped table-bordered">
+                <thead className="bg-primary">
+                <tr>
+                    <th scope="col">Account No.</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Balance</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>$1000</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>$3000</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>$5000</td>
+                </tr>
+                </tbody>
+            </table>
         </h1>
+
     );
 }
