6336e2c673b441ffc25630be5c8513b61bc72807
paul.v.estipona.ctr <paul_estipona@yahoo.com>
2023-06-02 02:47:02 -0400

    Added Styles to Card Component in context.js

--- home.js
+++ home.js
@@ -6,7 +6,7 @@ function Home(){
             header="BadBank"
             title="Welcome to the Bad Bank"
             text="For all your banking needs."
-            body={(<img src="bank.png" className="img-fluid" alt="responsive image"/>)}
+            body={(<img src="./Pics/money-saving-concept.jpg" className="img-fluid" alt="responsive image"/>)}
         />
     );
 }
